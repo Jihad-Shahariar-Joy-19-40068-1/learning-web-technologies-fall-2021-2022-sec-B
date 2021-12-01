63adfca965bf813c33c63f714e81d2a8ac85061f
Jihad-Shahariar-Joy-19-40068-1 <jihadshahariarjoy2211@gmail.com>
2021-12-02 00:02:37 +0600

    JQuery(not-working)11.59pm

--- FINAL_LAB_TASK_JQuery/script.js
+++ FINAL_LAB_TASK_JQuery/script.js
@@ -1,24 +1,31 @@
 
+	
 $('#submit').click(function(){
 	alert($('#name').val());
+	$('#name').val('');
 });
 
 $('#submit').click(function(){
 	alert($('#email').val());
+	$('#email').val('');
 });
 
 $('#submit').click(function(){
 	alert($('#gender').val());
+	$('#gender').val('');
 });
 
 $('#submit').click(function(){
 	alert($('#dob').val());
+	$('#dob').val('');
 });
 
 $('#submit').click(function(){
 	alert($('#degree').val());
+	$('#degree').val('');
 });
 
 $('#submit').click(function(){
 	alert($('#picture').val());
+	$('#picture').val('');
 });
